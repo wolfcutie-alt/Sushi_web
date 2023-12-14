b2e5654cab018b74444a5a44a8228d87172cb89a
Nathan-web24 <nathandoan24@gmail.com>
2023-12-15 07:16:55 +1300

    First Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+
+/*=============== SHOW MENU ===============*/
+
+
+
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */ 
+
+
+
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+
+
+    // When we click on each nav__link, we remove the show-menu class
+    
+
+
+
+
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+
+
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	
+
+
+
+
+
+
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+
+
+
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+
+
+// Previously selected topic (if user selected)
+
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+
+// We validate if the user previously chose a topic
+
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  
+
+
+
+
+
+// Activate / deactivate the theme manually with the button
+
+    // Add or remove the dark / icon theme
+    
+
+
+
+    // We save the theme and the current icon that the user chos
+
+
+
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
